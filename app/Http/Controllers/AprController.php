7bedd0c3c2cd65dac19f85d35df9e289a369115c
Samuel Theophylus Wieguna <wiegunasamuel@gmail.com>
2025-02-07 11:31:53 +0700

    first commit

---
+++ app/Http/Controllers/AprController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Request;
+
+class AprController extends Controller
+{
+    //
+    public function getApr(){
+        $url = 'https://serviceonemedia.wvi.org:8443/media/child';
+
+        $response = Http::withBasicAuth("", "")->withHeader("wv-version", "v500")->get($url);
+
+        dd($response);
+}
+
+}
