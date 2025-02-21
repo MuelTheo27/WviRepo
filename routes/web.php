424c4be7d5442c21cf291c49fc796707df9b4668
agnes <agnesgiovanniweinata@gmail.com>
2025-02-21 13:13:20 +0700

    asasa

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AprController;
 use App\Http\Controllers\FileController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TableController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,25 +16,25 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 // Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::prefix("/api")->group(function(){
-        Route::post("/upload/xslx", [FileController::class, 'uploadXslx'])->name("upload.xlsx");
-        Route::prefix("data")->group(function(){
-            Route::get("/index")->name("data.index");
-            Route::post("/search")->name("data.search");
-            Route::post("/sort")->name("data.sort");
-        });
-        Route::get("/download/xlsx")->name("download.xlsx");
+Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::prefix("/api")->group(function () {
+    Route::post("/upload/xslx", [FileController::class, 'uploadXslx'])->name("upload.xlsx");
+    Route::prefix("data")->group(function () {
+        Route::get("/index")->name("data.index");
+        Route::post("/search")->name("data.search");
+        Route::post("/sort")->name("data.sort");
     });
+    Route::get("/download/xlsx")->name("download.xlsx");
+});
 // });
 
 
-Route::get("/table", function(){
+Route::get("/table", function () {
     return view("tabledata");
 });
 // Route::get('/api/test',[FileController::class,'storeExcelData'])->name('Api.Test');
-require __DIR__.'/auth.php';
-
-Route::get('admin/dashboard',[AdminController::class,'index'])->name('admin.home');
+require __DIR__ . '/auth.php';
+Route::get("/test-data", [TableController::class, "getTablePage"]);
+Route::get('admin/dashboard', [AdminController::class, 'index'])->name('admin.home');
