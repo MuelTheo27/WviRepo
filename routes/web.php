64fa2fdb0200b5d5bfc667814150e67f9fcbe2d5
Ravel Athallah Widodo <ravelathallah5@gmail.com>
2025-02-21 12:52:11 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\AprController;
 use App\Http\Controllers\FileController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SponsorController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,6 +15,10 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/search-sponsors', [SponsorController::class, 'search'])->name('sponsors.search');
+Route::get('/table', [SponsorController::class, 'index'])->name('table.index');
+
 
 // Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -37,3 +43,5 @@ Route::get("/table", function(){
 require __DIR__.'/auth.php';
 
 Route::get('admin/dashboard',[AdminController::class,'index'])->name('admin.home');
+
+
